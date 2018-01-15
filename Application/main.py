42c95f861d7eb2b28a08a90a7e903c3e92bc2c65
Elisa Lescarret <e.s.lescarret@gmail.com>
2018-01-15 11:52:12 +0100

    add of second plot in main

--- Application/main.py
+++ Application/main.py
@@ -22,16 +22,23 @@ df_death_final = df_death_final.drop('Age-adjusted Death Rate', axis=1)
 #aggregate per cause of death per state and compute average number of deaths
 df_death_final = df_death_final.groupby(['Cause Name', 'State'],as_index=False).mean()
 #extract list of state
-stateList = df_death_final["State"].unique()
+state_list = df_death_final["State"].unique()
+#only keep interesting columns
+df_nutrition_final = df_nutrition[['YearStart', 'LocationDesc', 'Question', 'Data_Value', 'Gender']]
+#question list for the dropdown list
+question_list = df_nutrition_final["Question"].unique()
+
+
+
 #build the option list of the state dropdown list
 options_state_list = [{'label': 'All', 'value': 'All'}]
-options_state_list.extend([{'label': i, 'value': i} for i in stateList])
+options_state_list.extend([{'label': i, 'value': i} for i in state_list])
 app.layout = html.Div([
     html.Div([
 
         html.Div([
             dcc.Dropdown(
-                id='crossfilter-state',
+                id='options-state',
                 options = options_state_list,
                 value='All'
             )
@@ -39,11 +46,11 @@ app.layout = html.Div([
         style={'width': '49%', 'display': 'inline-block'}),
 
         html.Div([
-            # dcc.Dropdown(
-            #     id='crossfilter-question',
-            #     options=['Life expectancy at birth, total (years)'],
-            #     value='Life expectancy at birth, total (years)'
-            # )
+            dcc.Dropdown(
+                id='percentage-type',
+                options=[{'label': i, 'value': i} for i in question_list],
+                value='Percent of adults aged 18 years and older who have obesity'
+            )
         ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'})
     ], style={
         'borderBottom': 'thin lightgrey solid',
@@ -57,20 +64,24 @@ app.layout = html.Div([
             hoverData={'points': [{'customdata': 'Alabama'}]}
         )
     ], style={'width': '49%', 'display': 'inline-block', 'padding': '0 20'}),
-    html.Div(dcc.Slider(
-        id='crossfilter-year--slider',
-        min=yearList.min(),
-        max=yearList.max(),
-        value=yearList.max(),
-        step=None,
-        marks={str(year): str(year) for year in yearList}
-    ), style={'width': '49%', 'padding': '0px 20px 20px 20px'})
+    html.Div([
+        dcc.Graph(id='bar-chart'),
+        dcc.Graph(id='parallel-coordinate'),
+    ], style={'display': 'inline-block', 'width': '49%'}),
+    # html.Div(dcc.Slider(
+    #     id='crossfilter-year--slider',
+    #     min=yearList.min(),
+    #     max=yearList.max(),
+    #     value=yearList.max(),
+    #     step=None,
+    #     marks={str(year): str(year) for year in yearList}
+    # ), style={'width': '49%', 'padding': '0px 20px 20px 20px'})
 ])
 
 
 @app.callback(
     dash.dependencies.Output('crossfilter-indicator-scatter', 'figure'),
-[dash.dependencies.Input('crossfilter-state', 'value')])
+[dash.dependencies.Input('options-state', 'value')])
 def update_graph(state_input):
     #df_death_final = df_death_final.loc[df_death_final['Year'] >= year]
 
@@ -79,7 +90,7 @@ def update_graph(state_input):
         data=[ go.Scatter(
             x=df_death_final.loc[df_death_final['State'] == state_input]["Cause Name"],
             y=df_death_final.loc[df_death_final['State'] == state_input]["Deaths"],
-            name=state,
+            name=state_input,
             mode='markers',
             marker={
                 'size': 15,
@@ -87,7 +98,7 @@ def update_graph(state_input):
                 'line': {'width': 0.5, 'color': 'white'}
             })]
     else:
-        for state in stateList:
+        for state in state_list:
             trace = go.Scatter(
                 x=df_death_final.loc[df_death_final['State'] == state]["Cause Name"],
                 y=df_death_final.loc[df_death_final['State'] == state]["Deaths"],
@@ -103,62 +114,63 @@ def update_graph(state_input):
     return {
         'data': data,
         'layout': go.Layout(
-            title='Cause of death',
+           # title='Cause of death',
             xaxis=dict(title='Cause'),
             yaxis=dict(title='Avg'),
-            height=450
+            margin={'l': 40, 'b': 30, 't': 10, 'r': 0},
+            height=450,
+            hovermode='closest'
+        )
+    }
+
+@app.callback(
+    dash.dependencies.Output('bar-chart', 'figure'),
+    [dash.dependencies.Input('options-state', 'value'),
+     dash.dependencies.Input('percentage-type', 'value')])
+def bar_chart_percentage(state, percentage_type):
+    # selected from list
+    df_nutrition_question = df_nutrition_final[df_nutrition_final['Question'] == percentage_type]
+    # assume clicked state
+   # state = hoverData['points'][0]['customdata']
+    df_nutrition_question = df_nutrition_question.loc[df_nutrition_question['LocationDesc'] == state]
+    data = []
+    gender_list = df_nutrition_question["Gender"].unique()
+
+    for gender in gender_list:
+        trace = go.Bar(
+            x=df_nutrition_question.loc[df_nutrition_question['Gender'] == gender]["YearStart"],
+            y=df_nutrition_question.loc[df_nutrition_question['Gender'] == gender]["Data_Value"],
+            name=gender
+        )
+        data.append(trace)
+
+    return {
+        'data': data,
+        'layout': go.Layout(
+            barmode='stack',
+            #TODO show title but smaller
+            #title=percentage_type,
+            height= 225,
+            margin ={'l': 20, 'b': 30, 'r': 10, 't': 10},
         )
     }
 
 
-
-
-
-#
-# def create_time_series(dff, axis_type, title):
-#     return {
-#         'data': [go.Scatter(
-#             x=dff['Year'],
-#             y=dff['Value'],
-#             mode='lines+markers'
-#         )],
-#         'layout': {
-#             'height': 225,
-#             'margin': {'l': 20, 'b': 30, 'r': 10, 't': 10},
-#             'annotations': [{
-#                 'x': 0, 'y': 0.85, 'xanchor': 'left', 'yanchor': 'bottom',
-#                 'xref': 'paper', 'yref': 'paper', 'showarrow': False,
-#                 'align': 'left', 'bgcolor': 'rgba(255, 255, 255, 0.5)',
-#                 'text': title
-#             }],
-#             'yaxis': {'type': 'linear' if axis_type == 'Linear' else 'log'},
-#             'xaxis': {'showgrid': False}
-#         }
-#     }
-#
-#
-# @app.callback(
-#     dash.dependencies.Output('x-time-series', 'figure'),
-#     [dash.dependencies.Input('crossfilter-indicator-scatter', 'hoverData'),
-#      dash.dependencies.Input('crossfilter-xaxis-column', 'value'),
-#      dash.dependencies.Input('crossfilter-xaxis-type', 'value')])
-# def update_y_timeseries(hoverData, xaxis_column_name, axis_type):
-#     country_name = hoverData['points'][0]['customdata']
-#     dff = df[df['Country Name'] == country_name]
-#     dff = dff[dff['Indicator Name'] == xaxis_column_name]
-#     title = '<b>{}</b><br>{}'.format(country_name, xaxis_column_name)
-#     return create_time_series(dff, axis_type, title)
-#
-#
-# @app.callback(
-#     dash.dependencies.Output('y-time-series', 'figure'),
-#     [dash.dependencies.Input('crossfilter-indicator-scatter', 'hoverData'),
-#      dash.dependencies.Input('crossfilter-yaxis-column', 'value'),
-#      dash.dependencies.Input('crossfilter-yaxis-type', 'value')])
-# def update_x_timeseries(hoverData, yaxis_column_name, axis_type):
-#     dff = df[df['Country Name'] == hoverData['points'][0]['customdata']]
-#     dff = dff[dff['Indicator Name'] == yaxis_column_name]
-#     return create_time_series(dff, axis_type, yaxis_column_name)
+@app.callback(
+    dash.dependencies.Output('parallel-coordinate', 'figure'),
+    [dash.dependencies.Input('crossfilter-indicator-scatter', 'hoverData')])
+def update_x_timeseries(hoverData):
+    data = []
+    return {
+        'data': data,
+        'layout': go.Layout(
+        barmode='stack',
+       # xaxis=dict(title='year'),
+        #yaxis=dict(title='percentage')
+        height= 225,
+        margin = {'l': 20, 'b': 30, 'r': 10, 't': 10}
+    )
+    }
 
 
 if __name__ == '__main__':
