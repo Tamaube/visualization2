5a2df9df8a7cd114308891fe16bac7d0983d90a8
Elisa Lescarret <e.s.lescarret@gmail.com>
2018-01-15 12:31:37 +0100

    add third part in main

--- Application/main.py
+++ Application/main.py
@@ -3,6 +3,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 import plotly.graph_objs as go
 import pandas as pd
+import numpy as np
 
 app = dash.Dash()
 
@@ -13,8 +14,6 @@ df_death = pd.read_csv('Leading_Causes_Death.csv')
 df_death_final = df_death.loc[df_death['Year'] >= 2011]
 #remove 'All Causes'
 df_death_final = df_death.loc[df_death['Cause Name'] != 'All Causes']
-#save Year
-yearList = df_death_final["Year"].unique()
 #remove irrelevant columns
 df_death_final = df_death_final.drop('113 Cause Name', axis=1)
 df_death_final = df_death_final.drop('Age-adjusted Death Rate', axis=1)
@@ -25,10 +24,12 @@ df_death_final = df_death_final.groupby(['Cause Name', 'State'],as_index=False).
 state_list = df_death_final["State"].unique()
 #only keep interesting columns
 df_nutrition_final = df_nutrition[['YearStart', 'LocationDesc', 'Question', 'Data_Value', 'Gender']]
+#year list for slider
+year_list = df_nutrition_final["YearStart"].unique()
 #question list for the dropdown list
 question_list = df_nutrition_final["Question"].unique()
 
-
+#TODO: add the third plot, change title display, add slider for the year, put text when all is selected
 
 #build the option list of the state dropdown list
 options_state_list = [{'label': 'All', 'value': 'All'}]
@@ -68,28 +69,29 @@ app.layout = html.Div([
         dcc.Graph(id='bar-chart'),
         dcc.Graph(id='parallel-coordinate'),
     ], style={'display': 'inline-block', 'width': '49%'}),
-    # html.Div(dcc.Slider(
-    #     id='crossfilter-year--slider',
-    #     min=yearList.min(),
-    #     max=yearList.max(),
-    #     value=yearList.max(),
-    #     step=None,
-    #     marks={str(year): str(year) for year in yearList}
-    # ), style={'width': '49%', 'padding': '0px 20px 20px 20px'})
+    html.Div(dcc.Slider(
+        id='year-slider',
+        min=year_list.min(),
+        max=year_list.max(),
+        value=year_list.max(),
+        step=None,
+        marks={str(year): str(year) for year in year_list}
+    ), style={'width': '49%', 'padding': '0px 20px 20px 20px'})
 ])
 
 
 @app.callback(
     dash.dependencies.Output('crossfilter-indicator-scatter', 'figure'),
-[dash.dependencies.Input('options-state', 'value')])
-def update_graph(state_input):
-    #df_death_final = df_death_final.loc[df_death_final['Year'] >= year]
-
+[dash.dependencies.Input('options-state', 'value'),
+dash.dependencies.Input('year-slider', 'value')])
+def update_graph(state_input,year):
+    #df_death_graph = df_death_final[df_death_final['Year'] >= year]
+    df_death_graph= df_death_final
     data = []
     if(state_input != 'All'):
         data=[ go.Scatter(
-            x=df_death_final.loc[df_death_final['State'] == state_input]["Cause Name"],
-            y=df_death_final.loc[df_death_final['State'] == state_input]["Deaths"],
+            x=df_death_graph.loc[df_death_graph['State'] == state_input]["Cause Name"],
+            y=df_death_graph.loc[df_death_graph['State'] == state_input]["Deaths"],
             name=state_input,
             mode='markers',
             marker={
@@ -100,8 +102,8 @@ def update_graph(state_input):
     else:
         for state in state_list:
             trace = go.Scatter(
-                x=df_death_final.loc[df_death_final['State'] == state]["Cause Name"],
-                y=df_death_final.loc[df_death_final['State'] == state]["Deaths"],
+                x=df_death_graph.loc[df_death_graph['State'] == state]["Cause Name"],
+                y=df_death_graph.loc[df_death_graph['State'] == state]["Deaths"],
                 name=state,
                 mode='markers',
                 marker={
@@ -126,13 +128,15 @@ def update_graph(state_input):
 @app.callback(
     dash.dependencies.Output('bar-chart', 'figure'),
     [dash.dependencies.Input('options-state', 'value'),
-     dash.dependencies.Input('percentage-type', 'value')])
-def bar_chart_percentage(state, percentage_type):
-    # selected from list
-    df_nutrition_question = df_nutrition_final[df_nutrition_final['Question'] == percentage_type]
-    # assume clicked state
-   # state = hoverData['points'][0]['customdata']
-    df_nutrition_question = df_nutrition_question.loc[df_nutrition_question['LocationDesc'] == state]
+     dash.dependencies.Input('percentage-type', 'value'),
+     dash.dependencies.Input('year-slider', 'value')])
+def bar_chart_percentage(state, percentage_type, year):
+    # selected from lists
+    #df_nutrition_question = df_nutrition_final[df_nutrition_final['YearStart'] == year]
+    #df_nutrition_question = df_nutrition_question.loc[df_nutrition_question['LocationDesc'] == state]
+    df_nutrition_question = df_nutrition_final[df_nutrition_final['LocationDesc'] == state]
+    df_nutrition_question = df_nutrition_question.loc[df_nutrition_question['Question'] == percentage_type]
+
     data = []
     gender_list = df_nutrition_question["Gender"].unique()
 
@@ -158,18 +162,65 @@ def bar_chart_percentage(state, percentage_type):
 
 @app.callback(
     dash.dependencies.Output('parallel-coordinate', 'figure'),
-    [dash.dependencies.Input('crossfilter-indicator-scatter', 'hoverData')])
-def update_x_timeseries(hoverData):
-    data = []
+    [dash.dependencies.Input('options-state', 'value'),
+    dash.dependencies.Input('year-slider', 'value')
+     ])
+def update_pcp(state, year):
+    # select year
+    df_nutrition_pcp = df_nutrition_final[df_nutrition_final['YearStart'] == year]
+    df_nutrition_pcp = df_nutrition_pcp.loc[df_nutrition_pcp['LocationDesc'] == state]
+
+    df_nutrition_pcp = df_nutrition_pcp.replace(np.nan, 'unknown')
+
+    # convert categories into numerical vals
+    df_nutrition_pcp['Education'] = pd.Categorical(df_nutrition_pcp['Education'])
+    df_nutrition_pcp['EducationCode'] = df_nutrition_pcp['Education'].cat.codes
+    df_nutrition_pcp['Gender'] = pd.Categorical(df_nutrition_pcp['Gender'])
+    df_nutrition_pcp['GenderCode'] = df_nutrition_pcp['Gender'].cat.codes
+    df_nutrition_pcp['Income'] = pd.Categorical(df_nutrition_pcp['Income'])
+    df_nutrition_pcp['IncomeCode'] = df_nutrition_pcp['Income'].cat.codes
+    df_nutrition_pcp['Age(years)'] = pd.Categorical(df_nutrition_pcp['Age(years)'])
+    df_nutrition_pcp['Age(years)Code'] = df_nutrition_pcp['Age(years)'].cat.codes
+    df_nutrition_pcp['Race/Ethnicity'] = pd.Categorical(df_nutrition_pcp['Race/Ethnicity'])
+    df_nutrition_pcp['Race/EthnicityCode'] = df_nutrition_pcp['Race/Ethnicity'].cat.codes
+
+    data = [
+        go.Parcoords(
+            line=dict(color=year,
+                      colorscale='Jet',
+                      showscale=True,
+                      # reversescale=True,
+                      cmin=2011,
+                      cmax=2015),
+            dimensions=list([
+                dict(tickvals=[4, 3, 2, 1, 0],
+                     ticktext=['unknown', 'Less HS', 'HS', 'TS', 'C'],
+                     label='education categories', values=df_nutrition_pcp['EducationCode']),
+                dict(tickvals=[7, 6, 5, 4, 3, 2, 1, 0],
+                     ticktext=['unknown', '<15K', 'not reported', '>75k', '50k-75k', '35k-50k', '25k-35k', '15k-25k'],
+                     label='income levels', values=df_nutrition_pcp['IncomeCode']),
+                dict(tickvals=[2, 1, 0],
+                     ticktext=['unknown', 'M', 'F'],
+                     label='genders', values=df_nutrition_pcp['GenderCode']),
+                dict(tickvals=[6, 5, 4, 3, 2, 1, 0],
+                     ticktext=['unknown', '65+', '55-64', '45-54', '35-44', '25-34', '18-24'],
+                     label='age categories', values=df_nutrition_pcp['Age(years)Code']),
+                dict(tickvals=[8, 7, 6, 5, 4, 3, 2, 1, 0],
+                     ticktext=['unknown', 'other', 'NonHisp W', 'NonHisp B', 'Hisp', 'Hawaiian', 'Asian', 'AmericInd',
+                               '2+ races'],
+                     label='races', values=df_nutrition_pcp['Race/EthnicityCode'])
+            ])
+        )
+    ]
     return {
         'data': data,
         'layout': go.Layout(
-        barmode='stack',
-       # xaxis=dict(title='year'),
-        #yaxis=dict(title='percentage')
-        height= 225,
-        margin = {'l': 20, 'b': 30, 'r': 10, 't': 10}
-    )
+            plot_bgcolor='#E5E5E5',
+            paper_bgcolor='#E5E5E5',
+            showlegend=True,
+            height=225,
+            margin={'l': 20, 'b': 30, 'r': 10, 't': 10}
+        )
     }
 
 
