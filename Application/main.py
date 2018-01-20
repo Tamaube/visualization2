843f64ee528434de6739b7cd87d0c558e4ead2d2
AndradaPietraru <andra_pietraru@yahoo.com>
2018-01-20 15:49:16 +0100

    final cleanup

--- Application/main.py
+++ Application/main.py
@@ -1,3 +1,5 @@
+#only script used
+
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
@@ -5,62 +7,71 @@ import plotly.graph_objs as go
 import pandas as pd
 import numpy as np
 
-#create dash application
+# create dash application
 app = dash.Dash(__name__)
 server = app.server
 
-#read first dataset
+# read first dataset
 df_nutrition = pd.read_csv('Nutrition.csv')
 
-#replace long questions with a shorter equivalent
-df_nutrition["Question"].replace('Percent of adults who achieve at least 150 minutes a week of moderate-intensity aerobic physical activity or 75 minutes a week of vigorous-intensity aerobic activity (or an equivalent combination)', 'Percent of adults who workout moderately at least 150 mins a week',inplace=True)
-df_nutrition["Question"].replace('Percent of adults who achieve at least 150 minutes a week of moderate-intensity aerobic physical activity or 75 minutes a week of vigorous-intensity aerobic physical activity and engage in muscle-strengthening activities on 2 or more days a week','Percent of adults who achieve at workout 150 mins a week & muscle-strengthening activities 2+ days a week',inplace=True)
-df_nutrition["Question"].replace('Percent of adults who achieve at least 300 minutes a week of moderate-intensity aerobic physical activity or 150 minutes a week of vigorous-intensity aerobic activity (or an equivalent combination)','Percent of adults who workout moderately at least 300 mins a week',inplace=True)
+# replace long questions with a shorter equivalent
+df_nutrition["Question"].replace(
+    'Percent of adults who achieve at least 150 minutes a week of moderate-intensity aerobic physical activity or 75 minutes a week of vigorous-intensity aerobic activity (or an equivalent combination)',
+    'Percent of adults who workout moderately at least 150 mins a week', inplace=True)
+df_nutrition["Question"].replace(
+    'Percent of adults who achieve at least 150 minutes a week of moderate-intensity aerobic physical activity or 75 minutes a week of vigorous-intensity aerobic physical activity and engage in muscle-strengthening activities on 2 or more days a week',
+    'Percent of adults who achieve at workout 150 mins a week & muscle-strengthening activities 2+ days a week',
+    inplace=True)
+df_nutrition["Question"].replace(
+    'Percent of adults who achieve at least 300 minutes a week of moderate-intensity aerobic physical activity or 150 minutes a week of vigorous-intensity aerobic activity (or an equivalent combination)',
+    'Percent of adults who workout moderately at least 300 mins a week', inplace=True)
 
-#read second dataset
+# read second dataset
 df_death = pd.read_csv('Leading_Causes_Death.csv')
 
-#only keep data from 2011 until 2015
+# only keep data from 2011 until 2015
 df_death = df_death.loc[df_death['Year'] >= 2011]
 df_nutrition = df_nutrition.loc[df_nutrition['YearStart'] <= 2015]
 
-#remove Unitates States --> country, and we need only states
+# remove Unitates States --> country, and we need only states
 df_death_final = df_death.loc[df_death['State'] != 'United States']
 
-#remove 'All Causes'
+# remove 'All Causes'
 df_death_final = df_death_final.loc[df_death_final['Cause Name'] != 'All Causes']
 
-#remove irrelevant columns
+# remove irrelevant columns
 df_death_final = df_death_final.drop('113 Cause Name', axis=1)
 df_death_final = df_death_final.drop('Age-adjusted Death Rate', axis=1)
 
-#extract list of states
+# extract list of states
 state_list = df_death_final["State"].unique()
 
-#only keep interesting columns
-df_nutrition_final = df_nutrition[['YearStart', 'YearEnd','LocationDesc','Education','Income', 'Age(years)','Race/Ethnicity' , 'Question', 'Data_Value', 'Gender']]
+# only keep interesting columns
+df_nutrition_final = df_nutrition[
+    ['YearStart', 'YearEnd', 'LocationDesc', 'Education', 'Income', 'Age(years)', 'Race/Ethnicity', 'Question',
+     'Data_Value', 'Gender']]
 
-#year list for slider
+# year list for slider
 year_list = df_nutrition_final["YearStart"].unique()
 
-#question list for the dropdown list
+# question list for the dropdown list
 question_list = df_nutrition_final["Question"].unique()
 
-#build the option list of the state dropdown list
+# build the option list of the state dropdown list
 options_state_list = [{'label': 'All', 'value': 'All'}]
 options_state_list.extend([{'label': i, 'value': i} for i in state_list])
 
-#create layout
+# create layout
 app.layout = html.Div([
     html.Div([
         html.Div([
             dcc.Dropdown(
                 id='options-state',
-                options = options_state_list,
+                options=options_state_list,
                 value='All'
             )
         ],
-        style={'width': '49%', 'display': 'inline-block'}),
+            style={'width': '49%', 'display': 'inline-block'}),
 
         html.Div([
             dcc.Dropdown(
@@ -68,7 +79,7 @@ app.layout = html.Div([
                 options=[{'label': i, 'value': i} for i in question_list],
                 value='Percent of adults aged 18 years and older who have obesity'
             )
-        ], style={'width': '49%','float': 'right', 'display': 'inline-block'}),
+        ], style={'width': '49%', 'float': 'right', 'display': 'inline-block'}),
         html.Div(dcc.Slider(
             id='year-slider',
             min=year_list.min(),
@@ -95,20 +106,21 @@ app.layout = html.Div([
 
 ])
 
-#updating main plot
+
+# updating main plot
 @app.callback(
     dash.dependencies.Output('crossfilter-indicator-scatter', 'figure'),
     [dash.dependencies.Input('options-state', 'value'),
-    dash.dependencies.Input('year-slider', 'value')])
-def update_graph(state_input,year):
+     dash.dependencies.Input('year-slider', 'value')])
+def update_graph(state_input, year):
     df_death_graph = df_death_final.loc[df_death_final['Year'] <= year]
     # aggregate per cause of death per state and compute average number of deaths
     df_death_graph = df_death_graph.groupby(['Cause Name', 'State'], as_index=False).mean()
 
     data = []
 
-    if(state_input != 'All'):
-        data=[ go.Scatter(
+    if (state_input != 'All'):
+        data = [go.Scatter(
             x=df_death_graph.loc[df_death_graph['State'] == state_input]["Cause Name"],
             y=df_death_graph.loc[df_death_graph['State'] == state_input]["Deaths"],
             name=state_input,
@@ -138,11 +150,12 @@ def update_graph(state_input,year):
             margin={'l': 50, 'b': 120, 't': 10, 'r': 65},
             height=550,
             hovermode='closest',
-            yaxis=dict(title='avg no of deaths',zeroline=False)
+            yaxis=dict(title='avg no of deaths', zeroline=False)
         )
     }
 
-#updating second plot
+
+# updating second plot
 @app.callback(
     dash.dependencies.Output('bar-chart', 'figure'),
     [dash.dependencies.Input('options-state', 'value'),
@@ -151,20 +164,22 @@ def update_graph(state_input,year):
 def bar_chart_percentage(state, percentage_type, year):
     data = []
 
-    if(state == 'All'):
+    if (state == 'All'):
         return {
-        'data': data,
-        'layout': go.Layout(
-            height= 250,
-            margin ={'l': 40, 'b': 30, 'r': 10, 't': 10},
-            annotations=[dict(
-                x=0, y=0.85, xanchor='left', yanchor='bottom',
-                xref='paper', yref='paper', showarrow=False,
-                align='left', bgcolor='rgba(255, 255, 255, 0.5)',
-                text="You have to select a state in the left dropdown list", font=dict(family='sans serif', size=18)
-            )]
-        )
-    }
+            'data': data,
+            'layout': go.Layout(
+                height=250,
+                margin={'l': 40, 'b': 30, 'r': 10, 't': 10},
+                yaxis=dict(range=[1, 100], title='percentage'),
+                xaxis=dict(range=[2011, 2015], tickformat=',d'),
+                annotations=[dict(
+                    x=0, y=0.85, xanchor='left', yanchor='bottom',
+                    xref='paper', yref='paper', showarrow=False,
+                    align='left', bgcolor='rgba(255, 255, 255, 0.5)',
+                    text="You have to select a state in the left dropdown list", font=dict(family='sans serif', size=18)
+                )]
+            )
+        }
 
     df_nutrition_question = df_nutrition_final.copy()
     df_nutrition_question = df_nutrition_question[df_nutrition_question['YearStart'] <= year]
@@ -172,7 +187,7 @@ def bar_chart_percentage(state, percentage_type, year):
     df_nutrition_question = df_nutrition_question.loc[df_nutrition_question['Question'] == percentage_type]
 
     gender_list = df_nutrition_question["Gender"].unique()
-    gender_color_list = {"Female": '#FFB6C1', "Male" : '#ADD8E6', np.nan : '#FFFFFF' }
+    gender_color_list = {"Female": '#FFB6C1', "Male": '#ADD8E6', np.nan: '#FFFFFF'}
 
     for gender in gender_list:
         value = df_nutrition_question.loc[df_nutrition_question['Gender'] == gender]["Data_Value"]
@@ -180,33 +195,33 @@ def bar_chart_percentage(state, percentage_type, year):
             x=df_nutrition_question.loc[df_nutrition_question['Gender'] == gender]["YearStart"],
             y=value,
             name=gender,
-            marker=dict( color=gender_color_list[gender])
+            marker=dict(color=gender_color_list[gender])
         )
         data.append(trace)
-
 
     return {
         'data': data,
         'layout': go.Layout(
             barmode='stack',
-            height= 250,
-            margin ={'l': 40, 'b': 30, 'r': 10, 't': 10},
-            yaxis=dict(range=[1, 100],title='percentage'),
+            height=250,
+            margin={'l': 40, 'b': 30, 'r': 10, 't': 10},
+            yaxis=dict(range=[1, 100], title='percentage'),
             xaxis=dict(tickformat=',d'),
             annotations=[dict(
                 x=0, y=0.85, xanchor='left', yanchor='bottom',
                 xref='paper', yref='paper', showarrow=False,
                 align='left', bgcolor='rgba(255, 255, 255, 0.5)',
-                text="", font=dict(family='sans serif', size=18)#percentage_type
+                text="", font=dict(family='sans serif', size=18)  # percentage_type
             )]
         )
     }
 
-#updating third plot
+
+# updating third plot
 @app.callback(
     dash.dependencies.Output('line-chart', 'figure'),
     [dash.dependencies.Input('options-state', 'value'),
-    dash.dependencies.Input('year-slider', 'value')
+     dash.dependencies.Input('year-slider', 'value')
      ])
 def update_pcp(state, year):
     if (state == 'All'):
@@ -215,6 +230,8 @@ def update_pcp(state, year):
             'layout': go.Layout(
                 height=300,
                 margin={'l': 40, 'b': 30, 'r': 10, 't': 10},
+                yaxis=dict(title='count', range=[1, 100]),
+                xaxis=dict(range=[2011, 2015], tickformat=',d'),
                 annotations=[dict(
                     x=0, y=0.85, xanchor='left', yanchor='bottom',
                     xref='paper', yref='paper', showarrow=False,
@@ -258,10 +275,10 @@ def update_pcp(state, year):
     df_race = df_race.groupby(['LocationDesc', 'YearStart', 'Race/Ethnicity'], as_index=False).count()
 
     data = []
-    i=0
+    i = 0
 
     educationList = df_education["Education"].unique()
-    color_education=['#8697EA', '#4E67E5', '#3C50B2', '#232E66', '#3C4880']
+    color_education = ['#8697EA', '#4E67E5', '#3C50B2', '#232E66', '#3C4880']
     for ed in educationList:
         trace = go.Scatter(
             x=df_education.loc[df_education['Education'] == ed]["YearStart"],
@@ -271,24 +288,24 @@ def update_pcp(state, year):
             name="Ed_" + ed
         )
         data.append(trace)
-        i=i+1
+        i = i + 1
 
     incomeList = df_income["Income"].unique()
-    color_income = ['#FF4A6A' , '#FF210F', '#CC1A0C', '#7F1107', '#9A1D1E', '#FF5B5C']
-    i=0
+    color_income = ['#FF4A6A', '#FF210F', '#CC1A0C', '#7F1107', '#9A1D1E', '#FF5B5C']
+    i = 0
     for ed in incomeList:
         trace = go.Scatter(
             x=df_income.loc[df_income['Income'] == ed]["YearStart"],
-            #the count values are stores in YearEnd column
+            # the count values are stores in YearEnd column
             y=df_income.loc[df_income['Income'] == ed]["YearEnd"],
             mode='lines',
             name="In_" + ed,
             marker=dict(color=color_income[i])
         )
         data.append(trace)
-        i = i+1
+        i = i + 1
 
-    i=0
+    i = 0
     color_age = ['#E8A0EA', '#E267E5', '#E451E5', '#E530E5', '#B050B2', '#642E66', '#7F4B80']
     ageList = df_age["Age(years)"].unique()
     for ed in ageList:
@@ -296,15 +313,15 @@ def update_pcp(state, year):
             x=df_age.loc[df_age['Age(years)'] == ed]["YearStart"],
             y=df_age.loc[df_age['Age(years)'] == ed]["YearEnd"],
             mode='lines',
-            marker=dict( color=color_age[i]),
+            marker=dict(color=color_age[i]),
             name="Age_" + ed
         )
         data.append(trace)
-        i=i+1
+        i = i + 1
 
     raceList = df_race["Race/Ethnicity"].unique()
-    i=0
-    color_race=['#9FEA82', '#45E508', '#36B206', '#1F6603', '#338015', '#A4E52F', '#E5CE21', '#66630A', '#EA6C00']
+    i = 0
+    color_race = ['#9FEA82', '#45E508', '#36B206', '#1F6603', '#338015', '#A4E52F', '#E5CE21', '#66630A', '#EA6C00']
     for ed in raceList:
         trace = go.Scatter(
             x=df_race.loc[df_race['Race/Ethnicity'] == ed]["YearStart"],
@@ -314,15 +331,15 @@ def update_pcp(state, year):
             name="Race_" + ed
         )
         data.append(trace)
-        i=i+1
+        i = i + 1
 
     return {
         'data': data,
         'layout': go.Layout(
-            yaxis=dict(title='count'),
-            xaxis=dict(tickformat=',d'),
+            yaxis=dict(title='count',zeroline = False),
             height=300,
             margin={'l': 40, 'b': 30, 'r': 10, 't': 10},
+            xaxis=dict(tickformat=',d'),
             annotations=[dict(
                 x=0, y=1, xanchor='left', yanchor='bottom',
                 xref='paper', yref='paper', showarrow=False,
@@ -331,7 +348,6 @@ def update_pcp(state, year):
             )]
         )
     }
-
 
 if __name__ == '__main__':
     app.run_server()
